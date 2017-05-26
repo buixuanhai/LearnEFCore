241d43d8a76ba719d604e3e6a8f3f1a82db2f37f
Hai Bui <buixuanhai.it@gmail.com>
2017-05-27 02:35:53 +0700

    Add models

--- Domain/Samurai.cs
+++ Domain/Samurai.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace Domain
+namespace SamuraiApp.Domain
 {
-    public class Samurai
+  public class Samurai
+  {
+    public Samurai()
     {
-        
+      Quotes = new List<Quote>();
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Quote> Quotes { get; set; }
+    public int BattleId { get; set; }
     }
 }
